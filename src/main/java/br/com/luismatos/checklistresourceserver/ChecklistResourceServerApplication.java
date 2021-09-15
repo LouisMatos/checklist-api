c99e6c847fb398917e348766aaf27051e9fa9c68
Luis Matos <luismatos000@gmail.com>
2021-09-15 20:42:04 -0300

    Initial commit

---
+++ src/main/java/br/com/luismatos/checklistresourceserver/ChecklistResourceServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.luismatos.checklistresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luismatos/checklistresourceserver/ChecklistResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luismatos.checklistresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
